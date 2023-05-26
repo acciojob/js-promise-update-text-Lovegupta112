28db68d66e11b67fb4ec81f9b8d9889678a49cc0
lovegupta112 <loveguptaofficials@gmail.com>
2023-05-26 13:11:55 +0530

    Promise to Update HTML Element Text

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+
+let prom=new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+        resolve("Hello, world!");
+      },1000);
+})
+
+// console.log(prom);
+
+// setTimeout(()=>{
+//     console.log(prom);
+// },1500);
+
+prom.then((data)=>{
+//   console.log(data);
+  let div=document.getElementById('output');
+   div.innerText=data;
+
+})
+
